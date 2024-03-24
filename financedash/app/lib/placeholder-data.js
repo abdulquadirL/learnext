138fb430e2ff561e0b98a9223291ce8d98a3db1c
buharry <elanquad92@gmail.com>
2024-03-24 08:43:26 +0100

    add a util to the lib

---
+++ financedash/app/lib/placeholder-data.js
@@ -0,0 +1,51 @@
+const users = [
+    {
+        id: "",
+        name: "",
+        email: "",
+        password: ""
+    },
+    
+]
+
+const customers = [
+    {
+        id: "",
+        name: "",
+        email: "",
+        password: ""
+    },
+    {
+        id: "",
+        name: "",
+        email: "",
+        password: ""
+    },
+    {
+        id: "",
+        name: "",
+        email: "",
+        password: ""
+    },
+    {
+        id: "",
+        name: "",
+        email: "",
+        password: ""
+    },
+    {
+        id: "",
+        name: "",
+        email: "",
+        password: ""
+    }
+] 
+
+const invoices = [
+    {
+        customer_id: customers[0].id,
+        name: "",
+        status: "pending",
+        Date: ""
+    }
+]
