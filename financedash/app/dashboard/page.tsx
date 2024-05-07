dbf6a62f1a06dc5cb4e9fb35ac2cf1fbafc1397e
buharry <elanquad92@gmail.com>
2024-05-07 12:54:29 +0100

    modified dashboard page
    modified global.css
    
    modified homepage
    
    add logo
    
    add fonts file
    
    modified home style
    
    Add status file
    
    Added hero images

--- financedash/app/dashboard/page.tsx
+++ financedash/app/dashboard/page.tsx
@@ -1,3 +1,7 @@
 export default function Pages () {
-    return <p>Dashboard Pages</p>
+    return (
+        <div className="p-4 ">
+            <h2 className="font-bold text-xl">This is the finacial dashboard</h2>
+        </div>
+    )
 }
--- financedash/app/page.tsx
+++ financedash/app/page.tsx
@@ -1,27 +1,28 @@
 import Image from "next/image";
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css'
-//import { lusitana } from './ui/fonts';
+import { lusitana } from './ui/fonts';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex  flex-row h-20 shrink-0 items-end rounded-lg bg-green-500 p-4 md:h-52">
+        {/* <DealLogo /> */}
+        <div className="flex flex-row gap-4 md:px-14" >
+        <button className="bg-[#fff] text-green p-2 rounded-lg" >Signup</button>
+        <button className="bg-[#fff] text-green p-2 rounded-lg" >Login</button>
+        </div> 
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className={styles.shape} />
-          {/* <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p> */}
+          <h1 className="text-xl md:text-3xl font-bold text-green-600 gap-4">Secured, Boundless and Hassle-free Agrocommodity trading</h1>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-2xl md:leading-normal`}>
+            <strong> Deal</strong> is your one stop hub for transparent and secured Agrocommodity trading in Africa.
+          </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-green px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             {/* <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" /> */}
           </Link>
